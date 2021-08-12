4c4a87717a3d1eb94456a189e6b207754c682033
zazzaroa <aniello.zazzaro@ynap.com>
2021-08-12 23:25:25 +0200

    Moved streets in external Json Files

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
@@ -27,6 +28,7 @@ namespace ProCivReport
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/") }); //TODO: Mettere parametrico
             services.AddSingleton<WeatherForecastService>();
         }
 
--- wwwroot/clientSide.js
+++ wwwroot/clientSide.js
@@ -1,11 +1,16 @@
-﻿(function() {
+﻿window.disableCheckbox = (elementId, markers) => {
+    var elem = document.getElementById(elementId);
+    elem.setAttribute("disabled","true");
+    }
 
-    //window.clientSide = {
-    //    disableCheckbox: function(elementId, markers) {
-    //        var elem = document.getElementById(elementId);
-    //        elem.attr('disabled', 'true');
-    //        console.log(elem);
-    //        console.log("so entrato");
-    //    }
-    //}
-})
+window.saveInSession = (elementId) => {
+    var val = document.getElementById(elementId).value;
+    var streetsInSession = JSON.parse(window.sessionStorage.getItem('selected'));
+    var streets = streetsInSession != null ? streetsInSession : [];
+    streets.push(val);
+    window.sessionStorage.setItem('selected', JSON.stringify(streets));
+}
+
+window.sessionStorageClean = () => {
+    window.sessionStorage.clear();
+}
