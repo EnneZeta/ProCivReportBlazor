ecf5a71801c0edd284e5b0ad5d568fb17cd64030
unknown <djkomaster@gmail.com>
2021-08-12 09:01:31 +0200

    Initial Commit

---
+++ Paths.cs
@@ -0,0 +1,10 @@
+   using System.ComponentModel.DataAnnotations;
+   using System.Collections.Generic;
+   
+   public class Paths
+   {
+		public string NrPath {get; set;} 
+		
+		public List<string> StreetList {get; set;}
+   }
+   
