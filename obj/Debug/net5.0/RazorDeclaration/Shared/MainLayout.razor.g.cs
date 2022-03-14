91fbb5f8242e57eae18ecc0f2b0795dc5910a938
Gian Luca <autminric2002@yahoo.it>
2022-03-14 10:44:05 +0100

    modificato css per leggibilita'. modificato servicereport per impaginazione

--- obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,91 +13,91 @@ namespace ProCivReport.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 1 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 2 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 3 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 4 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 5 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 6 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 7 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 8 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 9 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 10 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using ProCivReport;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 11 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using ProCivReport.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 12 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 12 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using System.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 13 "C:\Users\zazzaroa\source\repos\ProCivReportBlazor\_Imports.razor"
+#line 13 "C:\Users\Gian Luca\Source\Repos\ProCivReportBlazor\_Imports.razor"
 using System.Data.SqlClient;
 
 #line default
